3729d7cdedb32b6b978e0737af63f6639ca29a44
Ilya Grigorev <89213248144@mail.ru>
2022-11-12 15:38:11 +0300

    program00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программу, которая 
+// 1 . на вход принимает число и 
+// 2 . выдаёт его квадрат (число умноженное на само себя).
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49c
+
+Console.WriteLine("Введение целое число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($" Квадрат числа {number} = {square} ");   
