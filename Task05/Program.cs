d3cd2d24c415b5a78f3aa7df9b6552c018234b49
Ilya Grigorev <89213248144@mail.ru>
2022-11-13 23:20:20 +0300

    Добавил задачи

---
+++ Task05/Program.cs
@@ -0,0 +1,18 @@
+﻿// 5. Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Ввидите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = -num;
+if (num >= 0)
+{
+    while (count <= num )
+    {
+       Console.Write(count + " "); 
+       count++;
+    }
+}
+else Console.WriteLine("Введено не верное число");
