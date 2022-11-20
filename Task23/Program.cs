381c484df71576de77bf07c7bc60bb7ea8b9935e
Ilya Grigorev <89213248144@mail.ru>
2022-11-21 00:54:04 +0300

    Решил задачу23

---
+++ Task23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, 
+// 1 которая принимает на вход число (N) 
+// 2 и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+
+
+	System.Console.WriteLine("Ввидите число:");
+	int number = Convert.ToInt32(Console.ReadLine());
+	
+	void Cubed  (int n )
+	 {
+	for (int i = 1; i <= n; i++) 
+	{
+	    int resolt = i * i * i;
+	    System.Console.WriteLine($"{i} | {resolt}") ;
+	
+    }
+     }
+    Cubed(number);
