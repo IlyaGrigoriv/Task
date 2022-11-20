89d7a552594492e6f42882bd3a71be43d17f10b5
Ilya Grigorev <89213248144@mail.ru>
2022-11-20 23:45:51 +0300

    убрал Task99

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -3,19 +3,20 @@
 // возможных координат точек в этой четверти (x и y).
 
 Console.WriteLine("Введите номер четверти");
-sting quarter = Convert.ToInt32(Console.ReadLine());
+int quarter = Convert.ToInt32(Console.ReadLine());
 
-string Quarter(string xc )
+int Quarter(int xc)
 {
+    string x,y;
     if (xc == 1) return "x > 0 && y > 0";
-    if (xc == 2) return "x > 0 && y < 0
-    if (xc == 3) return "x < 0 && y < 0";
-    if (xc == 4) return "x < 0 && y > 0";
+    if (xc == 2) return x > 0 && y < 0;
+    if (xc == 3) return x < 0 && y < 0;
+    if (xc == 4) return x < 0 && y > 0;
     return 0;
 }
 
-int quarter = Quarter(quarter);
-string result = quarter > 0
+int quarter1 = Quarter(quarter);
+int result = quarter1 > 0
         ? $"Указанные координаты соответствуют четверти -> {quarter}"
         : "Введены некорректные координаты";
 Console.WriteLine(result);
