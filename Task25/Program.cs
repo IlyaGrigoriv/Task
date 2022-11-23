e8537c53b1c87ce0211d5d3f9c16cd7d23cce544
Ilya Grigorev <89213248144@mail.ru>
2022-11-23 20:46:15 +0300

    задача 25

---
+++ Task25/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 25: Напишите цикл, 
+// 1 который принимает на вход два числа (A и B) 
+// 2 и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+System.Console.WriteLine("Ввидите число А");
+int A = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine("Ввидите число B");
+int B = Convert.ToInt32(System.Console.ReadLine());
+if (B < 0) B = -B;
+void ExpNumber(double numA, double numB)
+{
+
+    double result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result = result * numA;
+
+    }
+    System.Console.WriteLine($"{A}, {B}-->{result}");
+
+}
+ExpNumber(A, B);
+
+
+
+
+// void ExpNumber(double numA, double numB)
+// {
+//     double result = Math.Pow(numA, numB);
+//     System.Console.WriteLine(result);
+
+// }
+
+// ExpNumber(A,B);
--- Task30/Program.cs
+++ Task30/Program.cs
@@ -3,12 +3,22 @@
 // 2 заполненный нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
-int [] array = { 1, 0, 1, 1, 0, 1, 0, 0 };
+int[] array = new int[8];
+void RandowArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]=rnd.Next(0,2);
+    }
+}
+RandowArray(array);
+
 
 void PrintArray(int[] array)
 {
-    int count = array.Length;
-    for (int i = 0; i < count; i++)
+    //int count = array.Length;
+    for (int i = 0; i < array.Length; i++)
     {
         System.Console.Write($" {array[i]}");
 
