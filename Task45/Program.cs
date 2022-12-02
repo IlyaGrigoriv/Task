945e7a869ea652e006e679fe43cba9fb38932e1f
Ilya Grigorev <89213248144@mail.ru>
2022-12-02 12:38:58 +0300

    решил 45

---
+++ Task45/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+int size = 10;
+int min = 0;
+int max = 10;
+int[] NewArray(int Size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] = rnd.Next(min, max + 1);
+    return arr;
+
+}
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+
+    }
+    System.Console.Write("]");
+}
+
+int[] CopyArray(int[] arr)
+{
+    int[] arr1 = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr1[i] = arr[i];
+    }
+    return arr1;
+
+}
+
+int[] Array = NewArray(size, min, max);
+PrintArray(Array);
+int[] Array1 = CopyArray(Array);
+System.Console.WriteLine();
+Array1[0] = 2022;
+PrintArray(Array1);
